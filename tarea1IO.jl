e142ea672f5889eceabfce1b657f616628c37232
danelosky <43527612+danelosky@users.noreply.github.com>
2018-10-25 15:53:23 -0300

    Update tarea1IO.jl

--- tarea1IO.jl
+++ tarea1IO.jl
@@ -5,33 +5,37 @@ function geom(n,p)#funcion que se le entrega probabilidad de exito p, donde n es
 end
 
 function NumerodeToques(N,p)
-    V=zeros(2,25,N+1)
-    X=zeros(2,25,N)
+    pExito=0  #dsde 18-1
+    V=zeros(2,26,N+1)
+    X=zeros(2,26,N)
+
     for n = N:-1:1 #Personas que se entrevistan
         for s=0:1# 1 Si contraté a alguien antes de n o 0 si no
-            for t=0:1:25  #numero de toques que realizó el estudiante actual
+            for t=1:26  #numero de toques que realizó el estudiante actual
                 Vmax =-100000# Valor arbitrariamente bajo para que podamos guardar el primer resultado
                  # Si fuese minimización, fijaríamos un valor arbitrariamente alto
                 for x = 0:1 # 1 si contrato a estudiante n, 0 si no
-                    Vsum = 0#Variable para guardar la suma de valores de la esperanza
-                    for y = 0:1:25#calidad del estudiante que sera entrevistado
-                        Vsum = Vsum + V[s + (1-s)*x + 1, y+1,n+1]*geom(y,p)## formulacion de aguayo mejol , ESPERANZA
+                    Vsum = 0 #Variable para guardar la suma de valores de la esperanza
+                    for y = 1:26#calidad del estudiante que sera entrevistado
+                        Vsum = Vsum + V[s + (1-s)*x + 1, y,n+1]*geom(y,p)## formulacion de aguayo mejol , ESPERANZA
                     end
                     Vsum = (1-s)*((x*t) + Vsum)
                     if Vsum > Vmax
                         Vmax = Vsum
-                        V[s+1,t+1,n] = Vmax
-                        X[s+1,t+1,n] = x
+                        V[s+1,t,n] = Vmax
+                        X[s+1,t,n] = x
                     end
                 end
             end
         end
+
     end
     #en el primer "nodo"
-    probExito=0  #dsde 18-1
-    for i = 1:25
-        ProbExito = ProbExito + V[1,i,1]*geom(i,p)
+
+    for i = 1:1:25
+        pExito = pExito + V[1,i,1]*geom(i,p)
     end
-    return ProbExito
+    return pExito
 end
-NumerodeToques(12,0.3)
+
+NumerodeToques(11,0.1)
